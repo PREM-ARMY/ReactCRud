834ffdb3715c9963508ee4371519aeaed642fb2b
RufusprakashMM <rufusprakashkalla@makersmind.in>
2025-02-28 18:24:18 +0530

    done get

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,16 +1,14 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_error', 1);
-    error_reporting(E_ALL);
- $conn = mysqli_connect("localhost","root", "", "reactcrudmm");
+    
 //  $conn = mysqli_connect("localhost","root", "", "percentages");
+$conn = mysqli_connect('217.21.87.103','u237107965_reacttest','React@321$#','u237107965_reacttest');
 
-if($conn==true)
-{
-    echo '<script>alert("connection success")</script>';
-}
-else{
-    echo '<script>alert("connection failed")</script>';
-}
+// if($conn==true)
+// {
+//     echo '<script>alert("connection success")</script>';
+// }
+// else{
+//     echo '<script>alert("connection failed")</script>';
+// }
 
  ?>
